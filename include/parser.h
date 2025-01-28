333302ccb12cf1c12038b5bbf0c86a51de134055
Harsha Bhattacharyya <harshabhattacharyya510@duck.com>
2025-01-28 12:38:29 +0530

    0NULL0

--- include/parser.h
+++ include/parser.h
@@ -1,3 +1,26 @@
+/* Copyright (c) 2025, Harsha Bhattacharyya
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Patent Grant:
+
+Subject to the terms and conditions of this license, each copyright holder
+hereby grants to you a perpetual, worldwide, non-exclusive, no-charge,
+royalty-free, irrevocable (except as stated in this section) patent license to
+make, have made, use, offer to sell, sell, import, and otherwise transfer the
+software, where such license applies only to those patent claims licensable by
+such copyright holder that are necessarily infringed by their contribution(s)
+alone or by combination of their contribution(s) with the software to which such
+contribution(s) was submitted */
+
 #ifndef PARSER_H
 #define PARSER_H
 
