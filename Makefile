ff49ee970d7c76effbf3eafc71e4f109dcf54234
Harsha Bhattacharyya <harshabhattacharyya510@duck.com>
2025-02-10 16:29:11 +0530

    made install.pl better fixed compilation time compiler running out of memory

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@
 # Subject to the terms and conditions of this license, each copyright holder hereby grants to you a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable (except as stated in this section) patent license to make, have made, use, offer to sell, sell, import, and otherwise transfer the software, where such license applies only to those patent claims licensable by such copyright holder that are necessarily infringed by their contribution(s) alone or by combination of their contribution(s) with the software to which such contribution(s) was submitted 
 
 # Compiler and flags
-CXX = clang++
+CXX = g++
 EIGEN_INCLUDE = -Ieigen-3.4.0
 LLVM_CXXFLAGS = $(shell llvm-config --cxxflags)
 LLVM_LDFLAGS = $(shell llvm-config --ldflags --system-libs --libs core support)
--- install.pl
+++ install.pl
@@ -1,132 +1,57 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use File::Path qw(make_path);
-use Cwd qw(abs_path);
-use LWP::Simple qw(get);
+use File::Path qw(make_path remove_tree);
+use File::Copy;
 
-# Figlet Banner
-my $figlet_output;
-if (system("command -v figlet >/dev/null 2>&1") == 0) {
-    $figlet_output = `figlet -f slant CppScript`;
-} else {
-    $figlet_output = "CppScript";
-}
-
-print "\n";
-print $figlet_output;
-print "\n";
-
-# Eigen Configuration
+# Configuration
 my $eigen_version = "3.4.0";
-my $eigen_zip = "eigen-$eigen_version.zip";
-my $eigen_url = "https://gitlab.com/libeigen/eigen/-/archive/refs/tags/3.4.0.zip";
-my $eigen_dir = "eigen-$eigen_version";
+my $eigen_url = "https://gitlab.com/libeigen/eigen/-/archive/$eigen_version/eigen-$eigen_version.tar.gz";
+my $project_dir = `pwd`; chomp $project_dir;
+my $install_dir;
 
-# Download Eigen
-unless (-e $eigen_zip) {
-    print "Downloading Eigen $eigen_version...\n";
-    my $download_status = get $eigen_url, $eigen_zip;
-    if (!defined $download_status) {
-        die "Failed to download Eigen: $!\n";
-    }
-    print "Download complete.\n";
+# Determine environment
+my $uname_o = `uname -o`; chomp $uname_o;
+my $is_termux = $uname_o =~ /Android/ ? 1 : 0;
+
+# Set install directory
+if($is_termux) {
+    $install_dir = $ENV{'PREFIX'}."/bin";
 } else {
-    print "Eigen zip file already exists. Skipping Download.\n";
-}
-
-# Unzip Eigen
-unless (-d $eigen_dir) {
-    print "Unzipping Eigen...\n";
-    my $unzip_output = `unzip $eigen_zip 2>&1`;
-    my $unzip_exit_code = $?;
-
-    if ($unzip_exit_code != 0) {
-        die "Unzip failed: $unzip_output\n";
-    }
-
-    print "Unzip complete.\n";
-} else {
-    print "Eigen Directory already exists. Skipping Unzip.\n";
-}
-
-
-# Installation Configuration
-my $executable_name = "cpps";
-my $build_dir = ".";
-my $bin_dir;
-
-# Determine bin directory (Termux or normal)
-if ($ENV{TERMUX_PREFIX}) {
-    $bin_dir = "$ENV{TERMUX_PREFIX}/bin";
-} else {
-    $bin_dir = "/usr/local/bin";
-}
-
-# Create bin directory if it doesn't exist
-unless (-d $bin_dir) {
-    print "Creating bin directory: $bin_dir\n";
-    make_path($bin_dir) or die "Failed to create bin directory: $!\n";
-}
-
-# Change to the build directory
-chdir $build_dir or die "Failed to change to build directory '$build_dir': $!\n";
-my $build_dir_abs = abs_path($build_dir);
-
-print "Building in directory: $build_dir_abs\n";
-
-# Run make
-print "Running make...\n";
-my $make_output = `make 2>&1`;
-my $make_exit_code = $?;
-
-if ($make_exit_code != 0) {
-    print "Make failed:\n$make_output\n";
-    exit $make_exit_code;
-} else {
-    print "Make successful.\n";
-}
-
-# Check if the executable exists
-my $executable_path = "$build_dir/$executable_name";
-unless (-e $executable_path) {
-    die "Executable '$executable_name' not found in build directory '$build_dir'. Make sure it's built.\n";
-}
-
-# Install the executable
-my $target_path = "$bin_dir/$executable_name";
-
-print "Installing '$executable_name' to: $target_path\n";
-
-# Use 'install' command if available
-if (system("command -v install >/dev/null 2>&1") == 0) {
-    my $install_output = `install -m 0755 $executable_path $target_path 2>&1`;
-    my $install_exit_code = $?;
-
-    if ($install_exit_code != 0) {
-        print "Install failed: $install_output\nTrying cp...\n";
-        my $cp_output = `cp $executable_path $target_path 2>&1`;
-        my $cp_exit_code = $?;
-        if ($cp_exit_code != 0) {
-            die "Copy failed: $cp_output\n";
-        } else {
-            print "Copy successful.\n";
-        }
-    } else {
-        print "Install successful.\n";
-    }
-
-} else { # Install command not found, use cp
-    my $cp_output = `cp $executable_path $target_path 2>&1`;
-    my $cp_exit_code = $?;
-    if ($cp_exit_code != 0) {
-        die "Copy failed: $cp_output\n";
-    } else {
-        print "Copy successful.\n";
+    $install_dir = "/bin";
+    # Check for sudo access
+    if($> != 0 && !system("sudo -v >/dev/null 2>&1")) {
+        die "Error: sudo access required for Linux installation\n";
     }
 }
 
-print "Installation complete.\n";
+# Download and extract Eigen
+print "Downloading Eigen $eigen_version...\n";
+system("wget", "-O", "eigen.tar.gz", $eigen_url) == 0 or die "Download failed: $!";
 
-exit 0;
+print "Extracting Eigen...\n";
+system("tar", "-xzf", "eigen.tar.gz") == 0 or die "Extraction failed: $!";
+unlink "eigen.tar.gz";
 
+# Move Eigen headers
+my $eigen_dir = "$project_dir/eigen-$eigen_version";
+my $include_dir = "$project_dir/include";
+make_path($include_dir) unless -d $include_dir;
+
+# Build project
+print "Building project...\n";
+system("make", "clean") == 0 or die "Make clean failed: $!";
+system("make") == 0 or die "Build failed: $!";
+
+# Install executable
+print "Installing to $install_dir...\n";
+if($is_termux) {
+    move("cpps", "$install_dir/cpps") 
+        or die "Install failed: $!";
+} else {
+    system("sudo", "mv", "cpps", "$install_dir/") == 0 
+        or die "Install failed: $!";
+}
+
+print "Installation complete!\n";
+print "Executable installed to: $install_dir/cpps\n";
